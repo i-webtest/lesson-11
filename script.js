090356fea80a69d59ce045e24d35d69e5fb1a250
i-webtest <stasbass80@gmail.com>
2022-02-10 13:10:50 +0300

    добавил файл .jshintrc

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+"use strict";
 
+const btn = document.querySelector("button");
 
+const text = document.getElementById("text");
+const square = document.getElementById("square");
 
+const logger = function (event) {
+  console.log(event.type);
+  square.style.backgroundColor = "yellow";
+};
 
+text.addEventListener("input", logger);
 
+console.log(square);
