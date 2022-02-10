e2931008fb3dbe348bfb119304262ef7dbaaaa81
i-webtest <stasbass80@gmail.com>
2022-02-10 21:49:54 +0300

    выполнил урок №11

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
 "use strict";
 
-const btn = document.querySelector("button");
+const btn = document.getElementById("btn");
 
-const text = document.getElementById("text");
 const square = document.getElementById("square");
 
-const logger = function (event) {
-  console.log(event.type);
-  square.style.backgroundColor = "yellow";
-};
+const rangeElem = document.getElementById("range");
 
-text.addEventListener("input", logger);
+const circle = document.getElementById("circle");
 
+const rangeSpan = document.getElementById("range-span");
+
+circle.style.width = rangeElem.value;
+// circle.style.height = "100%";
+
+// const toggler = function (event) {
+//   event.target.classList.toggle("green");
+// };
+
+// const logger = function (event) {
+//   console.dir(event.target.value);
+//   btn.textContent = event.target.value;
+// };
+
+btn.addEventListener("click", function () {
+  let text = document.getElementById("text").value;
+  document.getElementById("square").style.backgroundColor = text;
+});
+
+document.getElementById("e_btn").style.display = "none";
+
+rangeElem.addEventListener("input", function () {
+  // let inputRange = document.getElementById("range").value;
+  // document.getElementById("circle").style.width = rangeElem.value;
+  circle.style.width = rangeElem.value + "%";
+  circle.style.height = rangeElem.value + "%";
+  // document.getElementById("circle").style.width = range;
+  rangeSpan.textContent = rangeElem.value + "%";
+});
+rangeSpan.textContent = rangeElem.value + "%";
+
+console.log(rangeSpan);
+console.log(window.getComputedStyle(circle).width);
+console.log(circle);
+// let onButtonClick = function () {
+//   btn.textContent = "жопа";
+// };
+
+// btn.addEventListener("click", onButtonClick);
+
+// text.addEventListener("input", logger);
+// square.addEventListener("click", toggler);
+
+// function myFunc() {
+//   let text = document.getElementById("text").value;
+//   document.getElementById("text-span").innerHTML = text;
+// }
+
+console.log(btn);
 console.log(square);
+console.log(rangeElem);
